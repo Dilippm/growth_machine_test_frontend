0eb94cc164e305e628ea6b529840ff4dbb7a431c
Dilippm <dilippm92@gmail.com>
2023-11-03 19:28:06 +0530

    functions finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
+import Webscrapper from './components/Webscrapper'
+import Table from './components/Table';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(0);
+
+  const handleButtonClick = () => {
+    setState(1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    { (state== 0) ? <div className='webscrap'> <Webscrapper onButtonClick={handleButtonClick} /></div>  : <div className='table'><Table/></div> }
     </>
   )
 }
